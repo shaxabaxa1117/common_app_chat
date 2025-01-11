63171245321e63ebf23c3e5b084ccc0ef4337f34
Wramggler <shaxabaxa17@gmail.com>
2025-01-11 19:22:54 +0500

    refactor: remove commented-out login method in logIn_page.dart

--- lib/pages/logIn_page.dart
+++ lib/pages/logIn_page.dart
@@ -10,23 +10,6 @@ class LoginPage extends StatelessWidget {
   final TextEditingController _passwordController = TextEditingController();
   void Function()? onTap;
 
-  // void login({required BuildContext context}) async {
-  //   final AuthService auth = AuthService();
-
-  //   try {
-  //     await auth.signInWithEmailPassword(
-  //         _emailController.text, _passwordController.text);
-  //   } catch (e) {
-  //     showDialog(
-  //       context: context,
-  //       builder: (context) {
-  //         return AlertDialog(
-  //           title: Text(e.toString()),
-  //         );
-  //       },
-  //     );
-  //   }
-  // }
 
   LoginPage({super.key, required this.onTap});
 
